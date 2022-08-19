e701994e612940147c0e28504b79fdad6d02ded5
Guilherme <guilhermehf23@gmail.com>
2022-08-18 22:03:20 -0300

    Segundo Commit

--- api.js
+++ api.js
@@ -1,2 +1,30 @@
-//Primeiro Commit
-console.log("Primeiro Commit")
+
+var express = require('express')
+var app = express()
+
+app.get('/produtos', function (req, res) {
+  res.send('Cadastrando Produtos!')
+})
+
+app.get('/produtos/:id', function (req, res) {
+    res.send('Buscando o produto')
+  })
+
+app.post('/', function (req, res) {
+    res.send('POST request to the homepage');
+  });
+
+  app.put('/produtos/:id', function (req, res) {
+    res.send('Atualizando o produto')
+  })
+
+  app.put('/produtos/:id', function (req, res) {
+    res.send('Atualizando o produto')
+  })
+
+  app.delete('/', function (req, res) {
+    res.send('Deletando o produto')
+  })
+
+app.listen(3000)
+
