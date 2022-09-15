0d5b0d1f18155347a26e6e947d0fb6f806db78ba
Guilherme <guilhermehf23@gmail.com>
2022-09-15 01:11:18 -0300

    Terceiro Commit

--- app.js
+++ app.js
@@ -4,31 +4,120 @@ const app = express()
 const port = 3000
 
 
-let listaProdutos = [];
-let geradorId = 1;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const produtos = [
+    {id:1, nome:"Arroz", preco: 4.5},
+    {id:2, nome:"Bolacha", preco:2.5},
+    {id:3, nome:"Doritos", preco:15},
+    {id:4, nome:"Chocolate", preco:5.8}];
+
+    let idGerado = 4;
+
+    let meuProduto = produto[0];
+    meuProduto.nome = "Arroz Doce";
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+/*Os Dados que vou receber vai vir no (req), e os dados que eu vou enviar vai ir no (res)*/
 app.get('/produtos', (req, res) => {
-    res.send('Listando Produtos!')
+    res.json(produtos)
 })
 
 app.post('/produtos', (req, res) =>{
-    res.send('Cadastrando Produtos!')
+    const novoProduto = req.body;
+    if(novoProduto && novoProduto.nome && novoProduto.preco){
+
+        novoProduto.id = idGerado++;
+    //idGerado = 6
+    produtos.push(novoProduto);
+    return res.status(201).json(novoProduto);
+
+    }
+    else{
+        return res.status(400).json({
+            Error:"Nome e/ou preco sao obrigatorio"
+        })
+    }
 })
 
 app.get('/produtos/:id', (req, res) => {
-    res.send('Buscando o Produto'+req.params.id)
+    const id= req.params.id;
+    const produtoEncontrado = produtos.find((produto) => {
+        return produto.id == id;
+    })
+
+    if(produtoEncontrado){
+        
+        return res.json(produtoEncontrado);
+    }
+    else{
+        return res.status(404).json(
+            {Erro:"Produto não encontrado"})
+
+    }
 })
 
 app.put('/produtos/:id', (req, res) => {
-    res.send('Atualizando o Produto'+req.params.id)
+    //Receber os dados que vem do request (req)
+
+    const id = req.params.id;
+    const produtoAlterar = req.body;
+
+    if(!produtoAlterar || !produtoAlterar.nome || produtoAlterar.preco){
+
+        return res.status(400).json({
+            Error:"Nome e/ou preco sao obrigatorio"
+        });
+
+    }
+
+    //Realizar/delegar a função de negocio
+
+    const produtoEncontrado = produtos.find(
+        (produto) => {
+            return produto.id == id;
+        }
+    )
+
+    if(produtoEncontrado){
+        produtoEncontrado.nome = produtoAlterar.nome;
+        produtoEncontrado.preco = produtoAlterar.preco;
+        return res.json(produtoEncontrado)
+    }
+    else{
+        return res.status(404).json(
+            {Erro:"Produto não encontrado"})
+
+    }
+
+   
+
+    res.send('Atualizando o produto'+req.params.id)
 })
 
 app.delete('/produtos/:id', (req, res) => {
-    res.send('Removendo o produto'+req.params.id)
+    const id= req.params.id;
+
+    const indiceProduto = produtos.findIndex(
+        (produto) => {
+            return produto.id == id;
+        }
+    
+    )
+
+    if(indiceProduto >= 0){
+        const produtoDeletado = produtos.slice(indiceProduto, 1)
+        return res.json(produtoDeletado);
+    }
+    else{
+        return res.status(404).json(
+            {Erro:"Produto não encontrado"})
+
+    }
+
 })
 
 app.listen(port, () => {
